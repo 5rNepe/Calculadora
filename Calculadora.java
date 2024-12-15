57b3c49315b4c2d0bf2d93615ee579de69227b64
5rNepe <hugoredondo2005@gmail.com>
2024-12-15 17:45:05 +0100

    4 metodos

--- Calculadora.java
+++ Calculadora.java
@@ -14,4 +14,11 @@ public class Calculadora {
         Integer multiplicacion = num1 * num2;
         return multiplicacion;
     }
+
+    public static double division(Integer num1, Integer num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo.");
+        }
+        return (double) num1 / num2;
+    }
 }
